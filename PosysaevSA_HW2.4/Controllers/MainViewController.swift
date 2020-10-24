5746f9c3da0396da96c7cde1b59f305c916680f1
Sergei Posysaev <fran@ustlab-1c.ru>
2020-10-24 20:19:04 +0300

    become + new file MainViewController

---
+++ PosysaevSA_HW2.4/Controllers/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  PosysaevSA_HW2.4
+//
+//  Created by Sergei Posysaev on 24.10.2020.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    var userAndPassword: (String, String)!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
